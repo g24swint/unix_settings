3733096ff2b0bf79b0c6a3d26e6f8053ffc911a9
Galen S. Swint, PhD <galen.swint@gmail.com>
2024-09-18 15:35:39 -0500

    ssh-agent turn on

---
+++ bash_sets/ssh-agents.sh
@@ -0,0 +1,4 @@
+# See https://web.archive.org/web/20210506080335/https://mah.everybody.org/docs/ssh
+
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/id_ed25519 ~/.ssh/lt_ssh/aws-lt-baryon.pem ~/.ssh/lt_ssh/aws-lt-id-prod-ec2-20240710.pem
